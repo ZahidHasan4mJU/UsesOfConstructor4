38239d0c7c624baaad7d5b1ef7c96f828724d5aa
ZahidHasan4mJU <responsible.zahid@yahoo.com>
2014-09-30 14:33:54 +0600

    added

---
+++ CircleAppWithConstrusctorNProperty/CircleApp/Circle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleApp
+{
+    class Circle
+    {
+        private double radius;
+
+
+        public double Radius
+        {
+            set
+            {
+                radius = value;
+            }
+            get
+            {
+                return radius;
+            }
+        }
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        public double getDiameter()
+        {
+            return (2*radius);
+        }
+
+        public double getPerimeter()
+        {
+            return (2*Math.PI*radius);
+        }
+
+        public double getArea()
+        {
+            return (Math.PI*radius*radius);
+        }
+    }
+}
---
+++ CircleAppWithConstrusctorNProperty/CircleApp/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace CircleApp
+{
+    partial class CircleAppForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radiusTextBox = new System.Windows.Forms.TextBox();
+            this.diameterTextBox = new System.Windows.Forms.TextBox();
+            this.perimeterTextBox = new System.Windows.Forms.TextBox();
+            this.areaTextBox = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Radius";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Area";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Perimeter";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Diameter";
+            // 
+            // radiusTextBox
+            // 
+            this.radiusTextBox.Location = new System.Drawing.Point(71, 17);
+            this.radiusTextBox.Name = "radiusTextBox";
+            this.radiusTextBox.Size = new System.Drawing.Size(135, 20);
+            this.radiusTextBox.TabIndex = 4;
+            // 
+            // diameterTextBox
+            // 
+            this.diameterTextBox.Location = new System.Drawing.Point(71, 79);
+            this.diameterTextBox.Name = "diameterTextBox";
+            this.diameterTextBox.Size = new System.Drawing.Size(135, 20);
+            this.diameterTextBox.TabIndex = 5;
+            // 
+            // perimeterTextBox
+            // 
+            this.perimeterTextBox.Location = new System.Drawing.Point(71, 105);
+            this.perimeterTextBox.Name = "perimeterTextBox";
+            this.perimeterTextBox.Size = new System.Drawing.Size(135, 20);
+            this.perimeterTextBox.TabIndex = 6;
+            // 
+            // areaTextBox
+            // 
+            this.areaTextBox.Location = new System.Drawing.Point(71, 131);
+            this.areaTextBox.Name = "areaTextBox";
+            this.areaTextBox.Size = new System.Drawing.Size(135, 20);
+            this.areaTextBox.TabIndex = 7;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(116, 43);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(90, 30);
+            this.calculateButton.TabIndex = 8;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.calculateButton);
+            this.groupBox1.Controls.Add(this.areaTextBox);
+            this.groupBox1.Controls.Add(this.perimeterTextBox);
+            this.groupBox1.Controls.Add(this.diameterTextBox);
+            this.groupBox1.Controls.Add(this.radiusTextBox);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(19, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(228, 168);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Circle App";
+            // 
+            // CircleAppForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(266, 207);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "CircleAppForm";
+            this.Text = "CircleAppUI";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox radiusTextBox;
+        private System.Windows.Forms.TextBox diameterTextBox;
+        private System.Windows.Forms.TextBox perimeterTextBox;
+        private System.Windows.Forms.TextBox areaTextBox;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
